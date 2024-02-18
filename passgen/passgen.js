1d7357a741d49a9df07c9c2124564e2aa50ca8d6
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2024-02-18 15:13:45 +0700

    shortening + something

--- passgen/passgen.js
+++ passgen/passgen.js
@@ -6,29 +6,26 @@ function pass(){
     var inlength = innerlength.value;
     var numlength = Number(inlength)
     var Char = "";
-    switch(inlength.length){
+    switch(numlength){
         case 0:
-        alert("Nothing detected");
+        alert("Not a number.");
         return;
     }
-    if (isNaN(numlength)) {
-        alert("Not a number")
-        return;
-    }else {
-        for (i=1; i<=numlength; i++){
-        var Chad = (Math.floor(Math.random()*100)+35)
-        if (Chad >= 127){Chad = 126}
-        var Asc = String.fromCharCode(Chad)
-        var Char = Char + Asc
-        }
-        document.getElementById("new pass")
-        .innerHTML +=
-        `<h4>
-        Your password is: ` + Char + `
-        </h4>`;
-        //alert("Your password is " + Char)
-    }
+       for (i=1; i<=numlength; i++){
+       var Chad = (Math.floor(Math.random()*100)+35)
+       if (Chad >= 127){Chad = 126}
+       var Asc = String.fromCharCode(Chad)
+       var Char = Char + Asc
+       }
+       document.getElementById("new pass")
+       .innerHTML +=
+       `<h4>
+       Your password is: ` + Char + `
+       </h4>`;
+       //alert("Your password is " + Char)
+   }
+
     
 
-}
+
 
