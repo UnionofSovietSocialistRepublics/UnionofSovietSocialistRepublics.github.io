5ee3574df707f8aff9c4a395b92fd2f96a4606ae
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2024-03-25 21:26:09 +0700

    more uiing also fail safe for buggy passgen

--- passgen/passgen.js
+++ passgen/passgen.js
@@ -5,11 +5,12 @@ function pass(){
     var innerlength = document.getElementById("input")
     var inlength = innerlength.value;
     var numlength = Number(inlength)
+    passgen(numlength)
+   }
+
+function passgen(numlength){
     var Char = "";
     var Asc = "";
-
-
-
     switch(isNaN(numlength)){
         case true:
         alert("Not a number");
@@ -23,16 +24,26 @@ function pass(){
        for (i=1; i<=numlength; i++){
        var Chad = (Math.floor(Math.random()*100)+35)
        if (Chad >= 127){Chad = 126}
+       //console.log(Chad)
        var Asc = String.fromCharCode(Chad)
        var Char = Char + Asc
        }
-       document.getElementById("new pass")
-       .innerHTML +=
-       `<h4>
-       Your password is: ` + Char + `
-       </h4>`;
        //alert("Your password is " + Char)
-   }
+       checker(numlength, Char)
+}
+
+function checker(numlength, Char){
+    console.log(Char)
+if (Char.indexOf('<') > -1){
+    passgen(numlength)
+}else {
+    document.getElementById("new pass")
+    .innerHTML +=
+    `<h4>
+    Your password is: ${Char}
+    </h4>`;
+}
+}
 
 function deletion(){
     document.getElementById("new pass")
